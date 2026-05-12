72d5bb25f4219433959b7bcdeaf174d57694c3f9
brage-andreas <bragerino+github@gmail.com>
2026-05-12 17:31:48 +0200

    Add TDT4100 ordinary spring exam 1

---
+++ tdt4100-exam-2025-1/src/main/java/com/bytebadger/assembly/part2/Anagram.java
@@ -0,0 +1,32 @@
+package com.bytebadger.assembly.part2;
+
+// TODO: Import relevant libraries
+
+public class Anagram {
+
+    /**
+     * This method checks if two strings are anagrams.
+     * 
+     * Two strings are anagrams if they contain the same characters
+     * in the same frequency, but possibly in a different order.
+     * The method ignores case and spaces.
+     * 
+     * Empty strings cannot be anagrams.
+     * Strings of different length cannot be anagrams.
+     * 
+     * Example:
+     * The words "listen" and "silent" are anagrams.
+     * 
+     * @param str1 the first input string
+     * @param str2 the second input string
+     * @return a boolean value indicating whether the two strings are anagrams
+     */
+    public boolean isAnagram(String str1, String str2) {
+
+        // TODO: Complete the method according to JavaDoc
+
+        return true; // Placeholder return statement, replace with actual logic
+
+    }
+}
+
---
+++ tdt4100-exam-2025-1/src/main/java/com/bytebadger/assembly/part2/CharacterFrequency.java
@@ -0,0 +1,23 @@
+package com.bytebadger.assembly.part2;
+
+import java.util.Map;
+
+// TODO: Import relevant libraries
+
+
+public class CharacterFrequency {
+
+    /**
+     * This method counts the frequency of each character in a given string.
+     * 
+     * @return a Map with characters as keys and their frequencies as values
+     */
+
+    public static Map<Character, Integer> countCharacterFrequency(String input) {
+
+        // TODO: Complete the method according to JavaDoc
+
+        return null;
+
+    }
+}
